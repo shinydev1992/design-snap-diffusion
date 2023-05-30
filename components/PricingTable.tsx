87127dfa4f68bdfab296f071549a42ae102dd679
shinydev1992 <shinydev1108@gmail.com>
2023-05-30 16:27:32 -0400

    5/30 - edit pricing table

--- components/PricingTable.tsx
+++ components/PricingTable.tsx
@@ -26,7 +26,7 @@ export default function PricingTable({ price, credit, tag }: PricingProps) {
           <div className="flex flex-col items-center ml-6 mb-6 mr-6 font-semibold tracking-widest">
             <p className="text-sm font-articulat mt-2 mb-1">Most popular</p>
             <p className="text-2xl font-arimo uppercase">${price} For {credit} Credits</p>
-            <p className="text-sm font-articulat mt-1">{price} room designs with any theme!</p>
+            <p className="text-sm font-articulat mt-1">{credit} room designs with any theme!</p>
             <button
               className="sm:bg-[#4C3D30] bg-[#99552A] rounded-full text-white text-lg font-arimo uppercase w-full px-8 py-2 mt-4"
               onClick={()=>{
@@ -40,7 +40,7 @@ export default function PricingTable({ price, credit, tag }: PricingProps) {
       ) : (
         <div className="flex flex-col items-center font-semibold tracking-widest">
           <p className="text-2xl font-arimo uppercase">${price} For {credit} Credits</p>
-          <p className="text-sm font-articulat mt-2">{price} room designs with any theme!</p>
+          <p className="text-sm font-articulat mt-2">{credit} room designs with any theme!</p>
           <button
             className="sm:bg-[#4C3D30] bg-[#99552A] rounded-full text-white text-lg font-arimo uppercase w-full px-8 py-2 mt-4"
           >

4a39315fca738c493f44954919f18e7d47e559ca
po <shinydev1108@gmail.com>
2023-06-26 22:39:10 -0700

    fix google analytics 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,21 +8,22 @@ import { SessionProvider } from "next-auth/react";
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-    {/* <Script  
-      strategy="lazyOnload"
-      async src="https://www.googletagmanager.com/gtag/js?id=G-08KF99X4ZE"></Script>
-      <Script strategy="lazyOnload">
-      {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-08KF99X4ZE', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-      </Script> */}
+      
     <SessionProvider session={session}>
-      <GoogleAnalytics />
+      <Script  
+        strategy="lazyOnload"
+        async src="https://www.googletagmanager.com/gtag/js?id=G-08KF99X4ZE"></Script>
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-08KF99X4ZE', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      {/* <GoogleAnalytics /> */}
       <Component {...pageProps} />
       <Analytics mode={'production'}/>
     </SessionProvider>

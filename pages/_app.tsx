f0333a1d9d144e79d85d7effbe22fb970819de51
po <shinydev1108@gmail.com>
2023-06-26 17:13:14 -0700

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      <Analytics />
+      <Analytics mode={'production'}/>
     </SessionProvider>
   );
 }

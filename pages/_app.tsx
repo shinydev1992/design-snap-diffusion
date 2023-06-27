5ca69466f7fc1a04a833983e08dc2e9ace9ca53e
po <shinydev1108@gmail.com>
2023-06-26 19:41:02 -0700

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
+    <>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-08KF99X4ZE"></Script>
+      <Script>
+      {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-08KF99X4ZE', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
     <SessionProvider session={session}>
       <Component {...pageProps} />
       <Analytics mode={'production'}/>
     </SessionProvider>
+    </>
   );
 }
 
